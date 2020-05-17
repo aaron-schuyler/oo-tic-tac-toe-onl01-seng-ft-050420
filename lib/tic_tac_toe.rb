b34d5fd5c62861ba12dd53bcc4534883f8bb8e27
Aaron Schuyler <asky28@gmail.com>
2020-05-17 18:12:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,6 @@ class TicTacToe
     @board = Array.new(9, " ")
   end
   def display_board
-  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -33,7 +32,14 @@ class TicTacToe
   end
   def turn()
     puts "It is #{current_player}'s turn! Enter a number between 1-9 to chose a spot."
-    
+    input = gets.chomp.to_i
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      puts "This move is invalid! Enter a number between 1-9 to chose a spot."
+    end
   end
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
@@ -72,7 +78,7 @@ class TicTacToe
   end
   def play 
     while !over? do
-      
+      turn
     end
     if won?
       puts "Player #{winner} has won the game!"
